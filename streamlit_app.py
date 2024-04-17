956ede5ca9a010a64ed5dbe2c1799668afa1c7f7
Jason Miles <jason.miles@bcs.org.uk>
2024-04-17 17:06:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,73 +1,44 @@
-# Jason Miles, April 20224
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-# Import Snowpark's Col Function
-from snowflake.snowpark.functions import col
-# Import core pythong random number generator
-import random
 
 # Write directly to the app
-st.title(":cup_with_straw: Customise Your Smoothie! :cup_with_straw:")
+st.title("Example Streamlit App :balloon:")
 st.write(
-    """Choose the Fruits you want to use in your Custom Smoothie!
-    """)
+    """Replace this example with your own code!
+    **And if you're new to Streamlit,** check
+    out our easy-to-follow guides at
+    [docs.streamlit.io](https://docs.streamlit.io).
+    """
+)
 
-name_on_order = st.text_input('Name on Smoothie:')
-st.write ('The name on your Smoothie will be', name_on_order)
-
+# Get the current credentials
 session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect (
-    'Choose up to 5 ingredients:'
-    ,my_dataframe
-    ,max_selections=5
-    )
-# print raw form - JSON array
-#st.text (ingredients_list)
+# Use an interactive slider to get user input
+hifives_val = st.slider(
+    "Number of high-fives in Q3",
+    min_value=0,
+    max_value=90,
+    value=60,
+    help="Use this to enter the number of high-fives you gave in Q3",
+)
 
-# initiatise empty order
-ingredients_string = ''
+#  Create an example dataframe
+#  Note: this is just some dummy data, but you can easily connect to your Snowflake data
+#  It is also possible to query data using raw SQL using session.sql() e.g. session.sql("select * from table")
+created_dataframe = session.create_dataframe(
+    [[50, 25, "Q1"], [20, 35, "Q2"], [hifives_val, 30, "Q3"]],
+    schema=["HIGH_FIVES", "FIST_BUMPS", "QUARTER"],
+)
 
-if ingredients_list:
-    ingredients_string = ''
+# Execute the query and convert it into a Pandas dataframe
+queried_data = created_dataframe.to_pandas()
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+# Create a simple bar chart
+# See docs.streamlit.io for more types of charts
+st.subheader("Number of high-fives")
+st.bar_chart(data=queried_data, x="QUARTER", y="HIGH_FIVES")
 
-# Use SQL to Insert
-# st.write(ingredients_string)
-# my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-#    values ('"""" + ingredients_string + """"')"""
-
-# print raw form - shows what is built
-#st.text (ingredients_string)
-
-
-# Use SQL to Insert
-#my_insert_stmt = """ insert into smoothies.public.orders.ingredients)
-#   values ('"""" + ingredients_string + """"','""""+name_on_order+ """"')"""
-
-
-#my_sql = f”insert into {table}”
-my_sql = orders_table = 'smoothies.public.orders'
-my_id=random.randint(1, 99999)
-
-my_insert_stmt = f"insert into {orders_table} values ({my_id},FALSE,'{name_on_order}','{ingredients_string}',CURRENT_TIMESTAMP())"
-
-
-#st.text(my_insert_stmt)
-
-#st.write(my_insert_stmt)
-#st.stop()
-
-time_to_insert = st.button('Submit Order')
-
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
-
-# shows content of tables - debugging 
-st.write(session.table(orders_table))
+st.subheader("Underlying data")
+st.dataframe(queried_data, use_container_width=True)
